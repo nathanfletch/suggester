207b0d178e3e5a025153777362348d0fbedb72c0
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-08-13 11:33:30 -0700

    Add business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,40 @@
-//business logic
-// function getLanguage () {
-//   let count = 0;
-
-// }
+// business logic
+function pyCount(answerArray) {
+  return answerArray.filter((answer) => answer === "a").length;
+}
+function jsCount(answerArray) {
+  return answerArray.filter((answer) => answer === "b").length;
+}
+function csCount(answerArray) {
+  return answerArray.filter((answer) => answer === "c").length;
+}
 
 //ui logic
 $(document).ready(function () {
-  $('#form1').submit(function (e) {
-    //a: python, b: js, c: C#
+  $("#form1").submit(function (e) {
     e.preventDefault();
-    const experience = $("input:radio[name=experience]:checked").val();
-    const goal = $("input:radio[name=goal]:checked").val();
-    const strictness = $("input:radio[name=strictness]:checked").val();
-    const car = $("input:radio[name=car]:checked").val();
-    const society = $("input:radio[name=society]:checked").val();
-    const semicolons = $("input:radio[name=semicolons]:checked").val();
-    const answers = [experience, goal, strictness, car, society, semicolons];
+
+    const experience = $("input:radio[name=experience]:checked").val(),
+      goal = $("input:radio[name=goal]:checked").val(),
+      strictness = $("input:radio[name=strictness]:checked").val(),
+      car = $("input:radio[name=car]:checked").val(),
+      society = $("input:radio[name=society]:checked").val(),
+      semicolons = $("input:radio[name=semicolons]:checked").val(),
+      answers = [experience, goal, strictness, car, society, semicolons];
+
+    const py = pyCount(answers),
+      js = jsCount(answers),
+      cs = csCount(answers);
+      
     console.log(answers);
+    console.log(py, js, cs);
 
-    // const language = getLanguage();
+    // if (py > js && py > cs) {
+    //   $("#language-result").text("JavaScript");
+    // } else if 
+      
+    $("#language-result").text("JavaScript");
 
-    $('#language-result').text('JavaScript');
-    $('#result').show();
-  })
-
-})
+    $("#result").show();
+  });
+});
